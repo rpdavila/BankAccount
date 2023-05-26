26de8f90da541835e071d8a1b025ddfb2a9d2e81
rpdavila <rafael.pietri@gmail.com>
2023-05-26 14:24:19 +0200

    Add project files.

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleTables;
+
+namespace BankAccount
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { 
+            get 
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount; 
+                }
+                return balance;
+            } 
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;            
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+
+        }
+
+         
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");  
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawel(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public void GetAccountHistory()
+        {
+            var table = new ConsoleTable("Date", "Amount", "Note");
+            foreach (var item in allTransactions)
+            {
+                table.AddRow(item.Date.ToShortDateString(), item.Amount, item.Notes);
+            }
+            
+            table.Write(Format.Alternative);
+            
+        }
+    }
+    
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Kendra", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with ${account.Balance}");
+            account.MakeWithdrawel(500, DateTime.Now, "Rent payment");
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            account.GetAccountHistory();
+        }
+    }
+}
