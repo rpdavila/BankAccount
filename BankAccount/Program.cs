0bd2280f39c114baafb16126e0577db38297b688
rpdavila <rafael.pietri@gmail.com>
2023-07-14 18:30:54 +0200

    removed commented code

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -25,19 +25,6 @@
                 userInput = Console.ReadLine();
                 Console.Clear();
             }
-            
-            
-            
-
-             
-
-            
-
-            //var account = new BankAccount("Kendra", 10000);
-            //Console.WriteLine($"Account {account.Number} was created for {account.Owner} with ${account.Balance}");
-            //account.MakeWithdrawel(500, DateTime.Now, "Rent payment");
-            //account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
-            //account.GetAccountHistory();
         }
     }
 }
